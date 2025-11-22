bf89e42fab4a21b2cb15490c1d8f09db3f69c63f
jerodwcox-Frontend <dnldfn76@gmail.com>
2025-11-22 19:31:15 +0700

    and they shall be saved

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('lkjoillulkjlkj')
+function openMenu() {
+document.body.classList += " menu--open"  
+}
+function closeMenu () {
+    document.body.classlist.remove('menu--open')
+}
+@media (max-width: 550px){
+.nav__links {
+    display: none;
+}
+}
